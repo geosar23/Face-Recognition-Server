7a724b99dbed2bb7b028b31f54b4c6d2297bfa72
George <geosar23@gmail.com>
2023-11-10 02:11:24 +0200

    DB details to .env

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ require('dotenv').config()
 const DB = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      port : 5432,
-      user : 'postgres',
-      password : 'postgres5991',
-      database : 'Face-Recognition-App'
+      host : process.env.DB_HOST,
+      port : process.env.DB_PORT,
+      user : process.env.DB_USER,
+      password : process.env.DB_PASSWORD,
+      database : process.env.DB_NAME
     }
 });
 
